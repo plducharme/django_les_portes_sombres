53a2b0ee11aed371f8e2e63e8d98c06bcfe90787
Pier Luc Ducharme <dutch80@gmail.com>
2024-02-26 16:23:01 -0500

    modifs scripts pour CI

--- config/configure.sh
+++ config/configure.sh
@@ -4,7 +4,7 @@ DJANGO_SETTINGS_MODULE='django_les_portes_sombres.settings'
 lsof -t -i tcp:8000 | xargs kill -9
 source venv/bin/activate
 python3 -m pip install -r config/requirements.txt
-cp -f config/test/settings.py .
+cp -f config/test/settings.py ./django_les_portes_sombres
 python3 manage.py makemigrations
 python3 manage.py migrate
 nohup python3 manage.py runserver 0.0.0.0:8000 --noreload
