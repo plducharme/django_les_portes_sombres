44883e279f90b75ca171e5156ed5605b3d2cab29
Pier Luc Ducharme <dutch80@gmail.com>
2024-02-26 16:00:55 -0500

    modifs scripts pour CI

--- config/configure.sh
+++ config/configure.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-lsof -t -i tcp:8000 | xargs kill -9
 PYTHONUNBUFFERED='1'
 DJANGO_SETTINGS_MODULE='django_les_portes_sombres.settings'
 source venv/bin/activate
@@ -7,5 +6,5 @@ python3 -m pip install -r config/requirements.txt
 cp -f config/test/settings.py .
 python3 manage.py makemigrations
 python3 manage.py migrate
-python3 manage.py runserver 0.0.0.0:8000 --noreload &
+python3 manage.py supervisor
 
--- config/test/settings.py
+++ config/test/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'aventure_web.apps.AventureWebConfig',
+    'djsupervisor',
 ]
 
 MIDDLEWARE = [
