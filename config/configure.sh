10cff298812e869090fbc1dd4975cc943fb0a52f
Pier Luc Ducharme <dutch80@gmail.com>
2024-02-26 15:17:38 -0500

    ajout scripts pour CI

---
+++ config/configure.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+PYTHONUNBUFFERED='1'
+DJANGO_SETTINGS_MODULE='django_les_portes_sombres.settings'
+source venv/bin/activate
+python3 -m pip install -r config/requirements.txt
+cp -f config/test/settings.py .
+python3 manage.py makemigrations
+python3 manage.py migrate
+python3 manage.py runserver
+
