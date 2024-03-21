ec364eb1b36a29448135bf38577129cc2017b38f
Pier Luc Ducharme <dutch80@gmail.com>
2024-03-21 17:40:45 -0400

    ajout support mysql

--- config/test/settings.py
+++ config/test/settings.py
@@ -79,9 +79,13 @@ WSGI_APPLICATION = 'django_les_portes_sombres.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": 'django.db.backends.mysql',
+        "NAME": "lps",
+        "USER": "root",
+        "PASSWORD": "lps_password",
+        "HOST": "127.0.0.1",
+        "PORT": "3306",
     }
 }
 
